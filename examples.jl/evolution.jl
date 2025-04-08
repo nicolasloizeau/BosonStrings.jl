cadd614dfd5a532b5cca6e7da8e0ba6e55d5341d
nicolasloizeau <nicolasloizeau@gmail.com>
2025-04-08 11:57:21 -0400

    single mode evolution example

---
+++ examples.jl/evolution.jl
@@ -0,0 +1,43 @@
+
+using BosonStrings
+using ProgressBars
+import PyPlot as plt
+
+
+function evolve(H, O, state, times)
+    result = []
+    dt = times[2] - times[1]
+    for t in ProgressBar(times)
+        push!(result, expect(O, state, state))  # save <0|O|0>
+        O = rk4(H, O, dt)  #preform one step of rk4
+    end
+    return real.(result)
+end
+
+
+
+
+# Parameters
+ω = 1.0 # bare frequency of the mode
+Ω = 2.5 # drive strength (time-independent in lab frame)
+
+# construct the Hamiltonian and number operator
+num = Operator(1)
+num += 1, (1, 1, 1)
+H = Operator(1)
+H += ω * num
+H += Ω, (1, 0, 1)
+H += Ω, (1, 1, 0)
+
+
+# Time evolution, time compute <0|num(t)|0>
+times = 0:0.1:100
+state = 0
+result = evolve(H, num, state, times)
+
+
+plt.plot(times, result)
+plt.xlabel("Time")
+plt.ylabel("Photon number expectation")
+plt.title("Driven, lossless single-mode oscillator")
+plt.show()
